5b5ab194247aa53447a67acaa7113d3378f16427
Olei Eto <teneeto@gmail.com>
2019-07-08 15:28:36 +0100

    feature:: posting data

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const bodyParser = require('body-parser');
 
 const app = express();
 const db = mongoose.connect('mongodb://localhost/bookAPI');
-const bookRouter = express.Router();
 const port = process.env.PORT || 3000;
 const Book = require('./models/bookmodel');
+const bookRouter = require('./routes/bookRouter')(Book);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
