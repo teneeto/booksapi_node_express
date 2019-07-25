d719455b8cfd4e6007a96bfef49207e6802718ae
Olei Eto <teneeto@gmail.com>
2019-07-25 12:24:12 +0100

    add middleware

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -22,21 +22,27 @@ function routes(Book) {
       });
     });
 
+  // implementing middlwares to intercept user requests before sending to server
+  bookRouter.use('/books/:bookId', (req, res, next) => {
+    Book.findById(req.params.bookId, (err, book) => {
+      if (err) {
+        return res.send(err);
+      }
+      if (book) {
+        req.book = book;
+        return next();
+      }
+      return res.sendStatus(404);
+    });
+  })
 
   bookRouter.route('/books/:bookId')
     .get((req, res) => {
-      Book.findById(req.params.bookId, (err, book) => {
-        if (err) {
-          return res.send(err);
-        }
-        return res.json(book);
-      });
+      res.json(req.book);
     })
     .put((req, res) => {
       Book.findById(req.params.bookId, (err, book) => {
-        if (err) {
-          return res.send(err);
-        }
+        const { book } = req;
         book.title = req.body.title;
         book.author = req.body.author;
         book.genre = req.body.genre;
