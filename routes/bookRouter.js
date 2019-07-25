1495606e1d0d1b39fd5a43e9bb37143bb0d69f88
Olei Eto <teneeto@gmail.com>
2019-07-25 13:07:01 +0100

    add patch and delete

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -37,19 +37,47 @@ function routes(Book) {
   })
 
   bookRouter.route('/books/:bookId')
-    .get((req, res) => {
-      res.json(req.book);
-    })
+    .get((req, res) => res.json(req.book))
     .put((req, res) => {
-      Book.findById(req.params.bookId, (err, book) => {
-        const { book } = req;
-        book.title = req.body.title;
-        book.author = req.body.author;
-        book.genre = req.body.genre;
-        book.read = req.body.read;
-        book.save();
+
+      const { book } = req;
+      book.title = req.body.title;
+      book.author = req.body.author;
+      book.genre = req.body.genre;
+      book.read = req.body.read;
+      req.book.save((err) => {
+        if (err) {
+          return res.send(err);
+        }
         return res.json(book);
+      })
+    })
+    .patch((req, res) => {
+      const { book } = req;
+      // eslint-disable-next-line no-uderscore-dangle
+      if (req.body._id) {
+        // eslint-disable-next-line no-uderscore-dangle
+        delete req.body._id;
+      }
+      Object.entries(req.body).forEach((item) => {
+        const key = item[0];
+        const value = item[1];
+        book[key] = value;
       });
+      req.book.save((err) => {
+        if (err) {
+          return res.send(err);
+        }
+        return res.json(book);
+      })
+    })
+    .delete((req, res) => {
+      req.book.remove((err) => {
+        if (err) {
+          return res.send(err);
+        }
+        return res.sendStatus(204);
+      })
     })
 
   return bookRouter;
